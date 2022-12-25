86a555379c87d0bf0ef303176dec5f56746bb046
Imbadimasa <chalganovdima@gmail.com>
2022-12-25 13:56:18 -0500

    Changed Hello World Message

--- git_testing_attempt_1/Program.cs
+++ git_testing_attempt_1/Program.cs
@@ -6,7 +6,7 @@ namespace git_testing_attempt_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Cruel World!");
         }
     }
 }
