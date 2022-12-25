addedaf44d60f4b9b85fd5594c7be1b91db2cac4
Imbadimasa <chalganovdima@gmail.com>
2022-12-25 13:50:58 -0500

    added C# program basic

---
+++ git_testing_attempt_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_testing_attempt_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
